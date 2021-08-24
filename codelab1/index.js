690241c4c6cfa588b8dffe2f3745724371126bc7
Bianca Corrêa <biancamonteiro22.06@gmail.com>
2021-08-24 13:48:46 -0300

    exercícios feitos até 4

--- codelab1/index.js
+++ codelab1/index.js
@@ -97,6 +97,7 @@ ${k4}      ${v4}
 
 //5. Faça um programa de cadastro de clientes que mostre um menu de opções e permita 
 //com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.
+// Vou fazer daqui a pouco
      
 
 
