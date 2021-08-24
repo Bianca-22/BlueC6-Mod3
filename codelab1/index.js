e76157fd7e7446bd7e86b507ec0e4e3d977bf9a4
Bianca Corrêa <biancamonteiro22.06@gmail.com>
2021-08-24 12:41:11 -0300

    primeiro cedelab do módulo 3

---
+++ codelab1/index.js
@@ -0,0 +1,110 @@
+// 1. Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!"
+const prompt = require("prompt-sync")();
+/*
+console.log(`
+"O primeiro programa a gente nunca esquece!"
+Não é o primeiro mas esse não será esquecido kkk <3
+`)
+*/
+
+// 2. Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda e o CEP e telefone na terceira.
+/*
+console.log(`Digite suas informações abaixo:`);
+
+const nome = prompt(`Nome Completo: `);
+const endereco = prompt(`Endereço: `);
+const cep = prompt(`CEP: `);
+const telefone = prompt(`Telefone: `);
+
+console.log(``);
+
+console.log(`Suas informações:`);
+console.log(`Nome: ${nome}`);
+console.log(`Endereço: ${endereco}`);
+console.log(`CEP: ${cep}`);
+console.log(`Telefone: ${telefone}`)
+*/
+
+// 3. Faça um programa que mostre na tela uma letra de música que você gosta (Se você utilizar template string o console obedece a quebra de linha).
+/*
+console.log(`
+Música: Take me to Church
+
+Refrão:
+
+No masters or kings when the ritual begins
+There is no sweeter innocence than our gentle sin
+In the madness and soil of that sad earthly scene
+Only then I am human
+Only then I am clean
+Oh, oh, Amen, Amen, Amen
+
+Take me to church
+I'll worship like a dog at the shrine of your lies
+I'll tell you my sins and you can sharpen your knife
+Offer me that deathless death
+Good God, let me give you my life
+Take me to church
+I'll worship like a dog at the shrine of your lies
+I'll tell you my sins and you can sharpen your knife
+Offer me that deathless death
+Good God, let me give you my life
+`)
+*/
+
+/*4. Você foi contratado ou contratada por uma escola pra fazer o sistema de boletim dos alunos.
+Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+```
+ALUNO (A)  NOTA
+=========  ====
+ALINE      9.0
+MÁRIO      DEZ
+SÉRGIO     4.5
+SHIRLEY    7.0
+```
+*/
+
+/*
+var boletim = {};
+var i = 0
+
+while(true){
+    boletim[prompt(`Nome do aluno: `)] = prompt(`Nota do aluno: `);
+    console.log(`---------------------------------`)
+    i += 1
+
+    if (i === 4){ break }
+}
+var k1 = Object.keys(boletim)[0];
+var v1 = boletim[k1];
+var k2 = Object.keys(boletim)[1];
+var v2 = boletim[k2];
+var k3 = Object.keys(boletim)[2];
+var v3 = boletim[k3];
+var k4 = Object.keys(boletim)[3];
+var v4 = boletim[k4];
+
+console.log(`
+ALUNO (A)  NOTA
+=========  ====
+${k1}      ${v1}
+${k2}      ${v2}
+${k3}      ${v3}
+${k4}      ${v4}
+`)
+*/
+
+//5. Faça um programa de cadastro de clientes que mostre um menu de opções e permita 
+//com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.
+     
+
+
+/*
+6. Escreva um programa que receba dois valores digitados pelo usuário(Utilize o objeto Math para arredondar o calculo):
+    * Quantidade de vida de um monstro (entre 10 e 50);
+    * Valor do ataque do jogador por turno (entre 5 e 10);
+    * Baseado nos valores digitados, exiba a quantidade de turnos que o jogador irá demorar para conseguir derrotar o monstro.
+    * O jogador irá derrotar o monstro em 8 turnos.
+*/
+
